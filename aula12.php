1ae3e912310ad6948a2eacfe23635a9362393e5e
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-11 10:51:12 -0200

    Aula 12, listando dados com forEach.

---
+++ aula12.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1>ForEach (callback[, thisObject])<h1>
+
+
+
+
+
+<script>
+
+var arr = ["a content of a blog", "news of article","recié for a cake", "programming tips"];
+arr.forEach(function(value,key){
+	document.write("<p>Key = " + key + "Value = " + value + "</p>");
+});
+</script>
+
+
+
+
+
+</body>
+</html>
+

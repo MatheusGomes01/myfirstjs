86bee13783a16af2da97c9ed92a9e38ac3ba2021
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-12 12:27:58 -0200

    Aula 15, trabalhando com For.

---
+++ aula15.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<h1>For</h1>
+
+<p>simple test:</p>
+
+<script>
+	for (var y = 0; y <= 50; y++){
+		document.write(y + "<br/>")
+	}
+</script>
+
+<p>advanced test:</p>
+
+
+
+<script>
+	var arr = ["a content of a blog","news of acticle","p for a cake","programming tips"];
+	for(var y = 0; y < arr.length; y++){
+		document.write(arr[y] +"<br/>");
+	}
+</script>
+
+</body>
+</html>

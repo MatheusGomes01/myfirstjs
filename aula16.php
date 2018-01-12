b74a91e519758fa22ee34856e58e9813b38d6331
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-12 12:36:53 -0200

    Aula 16, Do while and While, Do while análisa o bloco antes de executar, então funciona o sinal de menos, ao contrário do While.

---
+++ aula16.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Do While and While</h1>
+
+<p>Do While</p>
+
+<script>
+	var num = -10;
+	do{
+		num = num -1;
+		document.write(num);
+	}while(num >0);
+</script>
+
+<p>While:</p>
+
+<script>
+	var num = -10;
+	while(num > 0){
+		num = num -1;
+		document.write(num)
+	}
+</script>
+
+
+</body>
+</html>
